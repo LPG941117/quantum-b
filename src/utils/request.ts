df4f7c38132d54f6500719f9952e43f5a4e0aeb3
Shaun Li <sli@evos.com.au>
2021-09-27 17:41:49 +1000

    env set up

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,8 +1,9 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-const baseURL = "";
+const baseURL = process.env.NODE_ENV === "development" ? "" : "";
 const withCredentials = true;
 const timeout = 30000;
+
 /**
  * Requests a path, returning a promise.
  *
